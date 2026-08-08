8de1d1a912c9786363cda504c50186cd12567428
NaCryptoKe <Nahomcryptoketema@gmail.com>
2026-08-08 18:15:40 +0300

    Feature: made my system use a API standard enum output system

--- include/vector.h
+++ include/vector.h
@@ -3,6 +3,19 @@
 
 #include <stddef.h> // for size_t
 
+typedef enum 
+{
+    VEC_OK                  =  0,
+    VEC_ERR_OUT_OF_RANGE    = -1,
+    VEC_ERR_EMPTY           = -2,
+    VEC_ERR_ALLOC           = -3,
+    VEC_ERR_NULL_ARG        = -4,
+    VEC_ERR_EMPTY_VECTOR    = -5,
+    VEC_PRESENT             = -6,
+    VEC_NOT_PRESENT         = -7,
+    VEC_INVALID_SIZE        = -8
+} VectorStatus;
+
 typedef struct
 {
     void *data;         // Raw block of generic memory
@@ -11,24 +24,23 @@ typedef struct
     size_t elem_size;   // the data type's size
 } Vector;
 
-Vector* vector_init(size_t elem_size);
+Vector* vector_init(const size_t elem_size);
 
-void vector_push_back(Vector *vec, const void *value_ptr);
-void vector_push_front(Vector *vec, const void *value_ptr);
+VectorStatus vector_push_back(Vector *vec, const void *value_ptr);
+VectorStatus vector_push_front(Vector *vec, const void *value_ptr);
 
-int vector_pop_back(Vector *vec, void *out);
-int vector_pop_front(Vector *vec, void *out);
+VectorStatus vector_pop_back(Vector *vec, void *out);
+VectorStatus vector_pop_front(Vector *vec, void *out);
 
-void vector_insert(Vector *vec, size_t pos, const void *value_ptr);
-void vector_erase(Vector *vec, size_t pos);
-void vector_replace(Vector *vec, size_t init_pos, size_t end_pos, void *old_value_ptr, void *new_value_ptr);
-
-void vector_get(Vector *vec, size_t pos, void *out);
+VectorStatus vector_insert(Vector *vec, size_t pos, const void *value_ptr);
+VectorStatus vector_erase(Vector *vec, size_t pos);
+VectorStatus vector_replace(Vector *vec, size_t init_pos, size_t end_pos, void *old_value_ptr, void *new_value_ptr);
+VectorStatus vector_get(Vector *vec, size_t pos, void *out);
 
 size_t vector_search(Vector *vec, void *value_ptr);
-int vector_contains(Vector *vec, void *value_ptr);
+VectorStatus vector_contains(Vector *vec, void *value_ptr);
 
-void vector_clear(Vector *vec);
-void vector_destroy(Vector *vec);
+VectorStatus vector_clear(Vector *vec);
+VectorStatus vector_destroy(Vector *vec);
 
 #endif // VECTOR_H
--- src/vector.c
+++ src/vector.c
@@ -5,34 +5,66 @@
 #include <stdlib.h> // for realloc and free
 #include <string.h> // for memmove
 
-Vector* vector_init(size_t elem_size)
+VectorStatus vector_init_check(const size_t elem_size)
 {
+    if (elem_size == 0)
+        return VEC_INVALID_SIZE;
+    
+    return VEC_OK;
+}
+
+VectorStatus check(void *vec, void *value_ptr)
+{
+    if (vec == NULL || value_ptr == NULL)
+        return VEC_ERR_NULL_ARG;
+    
+    return VEC_OK;
+}
+
+Vector* vector_init(const size_t elem_size)
+{
+    if (vector_init_check(elem_size) != VEC_OK)
+        return NULL;
     Vector* vec = (Vector *)malloc(sizeof(Vector));
+    
+    // Just placeholder till I make check varaiadic.
+    int value = 42;
+
+    if (check(vec, &value) != VEC_OK)
+        return NULL;
+
     vec->capacity = 4;  // default starting point
     vec->size = 0;
     vec->elem_size = elem_size;
-    vec->data = malloc(vec->capacity * vec->elem_size);
 
-    // Logging info
-    PRINT("%zu", vec->capacity);
-    PRINT("%zu", vec->size);
-    PRINT("%zu", vec->elem_size);
+    vec->data = malloc(vec->capacity * vec->elem_size);
+    if (check(vec->data, &value) != VEC_OK)
+    {
+        free(vec);
+        return NULL;
+    }
 
     return vec;
 }
 
-void vector_push_back(Vector *vec, const void *value_ptr)
+VectorStatus vector_push_back(Vector *vec, const void *value_ptr)
 {
     LOG("Push Back\n");
+
+    if (vec == NULL || value_ptr == NULL)
+        return VEC_ERR_NULL_ARG;
     
     if (vec->size >= vec->capacity)
     {        
         vec->capacity = vec->capacity ? vec->capacity * 2 : 4;
-        void *new_data = realloc(vec->data, vec->capacity * vec->elem_size);
+        void *new_data = realloc(
+                                vec->data, 
+                                vec->capacity * vec->elem_size
+                            );
         if (new_data == NULL)   // Failed to get memory
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
@@ -40,13 +72,17 @@ void vector_push_back(Vector *vec, const void *value_ptr)
     char *target_address = (char *)vec->data + (vec->size * vec->elem_size);
     memcpy(target_address, value_ptr, vec->elem_size); // dereferences the target_address and changes the value of data
     vec->size++;
-    
+
+    return VEC_OK;    
 }
 
-void vector_push_front(Vector *vec, const void *value_ptr)
+VectorStatus vector_push_front(Vector *vec, const void *value_ptr)
 {
     LOG("Push Front\n");
     
+    if (vec == NULL || value_ptr == NULL)
+        return VEC_ERR_NULL_ARG;
+
     if (vec->size >= vec->capacity)
     {
         vec->capacity = vec->capacity ? vec->capacity * 2 : 4;
@@ -54,77 +90,101 @@ void vector_push_front(Vector *vec, const void *value_ptr)
         if (new_data == NULL)
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
     vec->size++;
-    memmove((char *)vec->data + vec->elem_size, (char *)vec->data, (vec->size - 1) * vec->elem_size);
+    memmove(
+        (char *)vec->data + vec->elem_size, 
+        (char *)vec->data, 
+        (vec->size - 1) * vec->elem_size
+    );
     memcpy(vec->data, value_ptr, vec->elem_size);
+
+    return VEC_OK;
 }
 
-int vector_pop_back(Vector *vec, void *out)
+VectorStatus vector_pop_back(Vector *vec, void *out)
 {
     LOG("Pop Back\n");
+    if (vec == NULL || out == NULL)
+        return VEC_ERR_NULL_ARG;
+        
     if (vec->size == 0)
-    {
-        return -1;  //placeholder for now
-    }
+        return VEC_ERR_EMPTY_VECTOR;
 
     vec->size--;
-    memcpy(out, (char *)vec->data + (vec ->size * vec->elem_size), vec->elem_size);
+    memcpy(
+        out,
+        (char *)vec->data + (vec ->size * vec->elem_size),
+        vec->elem_size
+    );
     
-    if (vec->size <= vec->capacity / 4 && vec->capacity > 4)
+    if (
+        vec->size <= vec->capacity / 4 && 
+        vec->capacity > 4
+    )
     {
         vec->capacity /= 2;
-        if (vec->capacity < 4)
-            vec->capacity = 4;
         void *new_data = realloc(vec->data, vec->capacity * vec->elem_size);
         if (new_data == NULL)
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
-    return 1;
+
+    return VEC_OK;
 }
 
-int vector_pop_front(Vector *vec, void *out)
+VectorStatus vector_pop_front(Vector *vec, void *out)
 {
     LOG("Pop Front\n");
+    if (vec == NULL || out == NULL)
+        return VEC_ERR_NULL_ARG;
+
     if (vec->size == 0)
-    {
-        return -1;  //placeholder for now
-    }
+        return VEC_ERR_EMPTY_VECTOR;
 
     vec->size--;
-    memcpy(out, vec->data, vec->elem_size);
-    memmove((char *)vec->data, (char *)vec->data + vec->elem_size, vec->size * vec->elem_size);
+    memcpy(
+        out, 
+        vec->data, 
+        vec->elem_size
+    );
+    memmove(
+        (char *)vec->data, 
+        (char *)vec->data + vec->elem_size, 
+        vec->size * vec->elem_size
+    );
 
-    if (vec->size <= vec->capacity / 4 && vec->capacity > 4)
+    if (
+        vec->size <= vec->capacity / 4 && 
+        vec->capacity > 4
+    )
     {
         vec->capacity /= 2;
-        if (vec->capacity <= 4)
-            vec->capacity = 4;
         void *new_data = realloc(vec->data, vec->capacity * vec ->elem_size);
         if (new_data == NULL)
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
-    return 1;
+    return VEC_OK;
 }
 
-void vector_insert(Vector *vec, size_t pos, const void *value_ptr)
+VectorStatus vector_insert(Vector *vec, size_t pos, const void *value_ptr)
 {
     LOG("Vector Insert\n");
+    if (vec == NULL || value_ptr == NULL)
+        return VEC_ERR_NULL_ARG;
+
     if (pos > vec->size)
-    {
-        return;
-    }
+        return VEC_ERR_OUT_OF_RANGE;
 
     if (vec->size >= vec->capacity)
     {
@@ -133,23 +193,41 @@ void vector_insert(Vector *vec, size_t pos, const void *value_ptr)
         if (new_data == NULL)
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
+
     vec->size++;
-    memmove((char *)vec->data + (pos * vec->elem_size) + vec->elem_size, (char *)vec->data + (pos * vec->elem_size), (vec->size - pos - 1) * vec->elem_size);
-    memcpy((char *)vec->data + (pos * vec->elem_size), value_ptr, vec->elem_size);
+    memmove(
+        (char *)vec->data + (pos * vec->elem_size) + vec->elem_size,
+        (char *)vec->data + (pos * vec->elem_size),
+        (vec->size - pos - 1) * vec->elem_size
+    );
+    memcpy(
+        (char *)vec->data + (pos * vec->elem_size),
+        value_ptr,
+        vec->elem_size
+    );
+
+    return VEC_OK;
 }
 
-void vector_erase(Vector *vec, size_t pos)
+VectorStatus vector_erase(Vector *vec, size_t pos)
 {
+    if (vec == NULL)
+        return VEC_ERR_NULL_ARG;
+
     if (pos >= vec->size)
-    {
-        return;
-    }
-    memmove((char *)vec->data + (pos * vec->elem_size), (char *)vec->data + (pos * vec->elem_size) + vec->elem_size, (vec->size - pos - 1) * vec->elem_size);
+        return VEC_ERR_OUT_OF_RANGE;
+
+    memmove(
+        (char *)vec->data + (pos * vec->elem_size),
+        (char *)vec->data + (pos * vec->elem_size) + vec->elem_size,
+        (vec->size - pos - 1) * vec->elem_size
+    );
     vec->size--;
+
     if (vec->size <= vec->capacity/4 && vec->capacity > 4)
     {
         vec->capacity /= 2;
@@ -157,42 +235,65 @@ void vector_erase(Vector *vec, size_t pos)
         if (new_data == NULL)
         {
             fprintf(stderr, "Fatal: Out of memory\n");
-            exit(EXIT_FAILURE);
+            return VEC_ERR_ALLOC;
         }
         vec->data = new_data;
     }
+    return VEC_OK;
 }
 
-void vector_replace(Vector *vec, size_t init_pos, size_t end_pos, void *old_value_ptr, void *new_value_ptr)
+VectorStatus vector_replace(Vector *vec, size_t init_pos, size_t end_pos, void *old_value_ptr, void *new_value_ptr)
 {
     LOG("Vector Replace\n");
-    if (init_pos >= vec->size ||
+    if (vec == NULL || old_value_ptr == NULL || new_value_ptr == 0)
+        return VEC_ERR_NULL_ARG;
+
+    if (
+        init_pos >= vec->size ||
         end_pos >= vec->size ||
-        init_pos > end_pos)
-    {
-        return;
-    }
+        init_pos > end_pos
+    )
+        return VEC_ERR_OUT_OF_RANGE;
 
     for (size_t i = init_pos; i <= end_pos; i++) {
-        if (memcmp((char *)vec->data + (i * vec->elem_size), old_value_ptr, vec->elem_size) == 0)
+        if (
+            memcmp(
+                (char *)vec->data + (i * vec->elem_size),
+                old_value_ptr,
+                vec->elem_size
+            ) == 0)
         {
-            memcpy((char *)vec->data + (i * vec->elem_size), new_value_ptr, vec->elem_size);
+            memcpy(
+                (char *)vec->data + (i * vec->elem_size), 
+                new_value_ptr, 
+                vec->elem_size
+            );
         }
     }
+
+    return VEC_OK;
 }
 
-void vector_get(Vector *vec, size_t pos, void *out)
+VectorStatus vector_get(Vector *vec, size_t pos, void *out)
 {
     LOG("Vector Get\n");
+    if (vec == NULL || out == NULL)
+        return VEC_ERR_NULL_ARG;
+
     if (pos >= vec->size)
-        return;
+        return VEC_ERR_OUT_OF_RANGE;
     
     memcpy(out, (char *)vec->data + (pos * vec->elem_size), vec->elem_size);
+
+    return VEC_OK;
 }
 
 size_t vector_search(Vector *vec, void *value_ptr)
 {
     LOG("Vector Search\n");
+    if (check(vec, value_ptr) != VEC_OK)
+        return -1;
+
     for (size_t i = 0; i < vec->size; i++)
     {
         if (memcmp((char *)vec->data + (i * vec->elem_size), value_ptr, vec->elem_size) == 0)
@@ -203,24 +304,37 @@ size_t vector_search(Vector *vec, void *value_ptr)
     return -1;
 }
 
-int vector_contains(Vector *vec, void *value_ptr)
+VectorStatus vector_contains(Vector *vec, void *value_ptr)
 {
     LOG("Vector Contatains\n");
-    return (int)(vector_search(vec, value_ptr) != (size_t)-1);
+    if (vec == NULL || value_ptr == NULL)
+        return VEC_ERR_NULL_ARG;
+
+    return vector_search(vec, value_ptr) != (size_t)-1 ? VEC_PRESENT : VEC_NOT_PRESENT;
 }
 
-void vector_clear(Vector *vec)
+VectorStatus vector_clear(Vector *vec)
 {
     LOG("Vector Clear\n");
+    if (vec == NULL)
+        return VEC_ERR_NULL_ARG;
+
     free(vec->data);
     vec->data = NULL;
     vec->capacity = 0;
     vec->size = 0;
+
+    return VEC_OK;
 }
 
-void vector_destroy(Vector *vec)
+VectorStatus vector_destroy(Vector *vec)
 {
     LOG("Vector Destroyed\n");
+    if (vec == NULL)
+        return VEC_ERR_NULL_ARG;
+
     free(vec->data);
     free(vec);
+
+    return VEC_OK;
 }
